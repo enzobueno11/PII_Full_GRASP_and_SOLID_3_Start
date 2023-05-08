67aa9c417180c948008018a01ae9aad2556fc49f
enzobueno11 <127800256+enzobueno11@users.noreply.github.com>
2023-05-08 20:59:44 -0300

    ejercicio

--- src/Library/AllInOnePrinter.cs
+++ src/Library/AllInOnePrinter.cs
@@ -14,18 +14,4 @@ namespace Full_GRASP_And_SOLID.Library
         File
     }
 
-    public class AllInOnePrinter
-    {
-        public void PrintRecipe(Recipe recipe, Destination destination)
-        {
-            if (destination == Destination.Console)
-            {
-                Console.WriteLine(recipe.GetTextToPrint());
-            }
-            else
-            {
-                File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
-            }
-        }
-    }
 }
---
+++ src/Library/IPrinter.cs
@@ -0,0 +1,9 @@
+namespace Full_GRASP_And_SOLID.Library
+{
+using System;
+
+public interface IPrinter
+{
+        void PrintTicket(Recipe recipe);
+}
+}
